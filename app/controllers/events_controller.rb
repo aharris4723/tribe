e39b40f7f71d72ae52b06c47d214a6361d34c006
aharris4723 <aharris1727@gmail.com>
2018-03-29 15:31:18 -0400

    google button created

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,7 +12,20 @@ class EventsController < ApplicationController
    @comments = Comment.all
    @comment = Comment.new  
    @event = Event.find(params[:id])
-   
+   if session[:authorization] == nil
+        else
+            client = Signet::OAuth2::Client.new(client_options)
+            client.update!(session[:authorization])
+            tokenRefresh
+            service = Google::Apis::CalendarV3::CalendarService.new
+            service.authorization = client
+            if service.authorization.expires_at < Time.now 
+            else 
+                @calendar_list = service.list_calendar_lists
+                @test = @calendar_list.items.first.id 
+            end 
+
+   end
   end
 
  
@@ -20,6 +33,66 @@ class EventsController < ApplicationController
     @event = Event.new
     
   end
+  def redirect
+    client = Signet::OAuth2::Client.new(client_options)
+
+    redirect_to client.authorization_uri.to_s
+  end
+   def callback
+         client = Signet::OAuth2::Client.new(client_options)
+         client.code = params[:code]
+         response = client.fetch_access_token!
+         
+         session[:authorization] = response
+         session[:authorization][:code] = params[:code]
+         redirect_to "/events/#{current_user.id}"
+     end 
+   
+    def calendars
+    client = Signet::OAuth2::Client.new(client_options)
+    client.update!(session[:authorization])
+
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.authorization = client
+
+    @calendar_list = service.list_calendar_lists
+    rescue Google::Apis::AuthorizationError
+    response = client.refresh!
+
+    session[:authorization] = session[:authorization].merge(response)
+
+    retry
+  
+   end 
+    def events
+    client = Signet::OAuth2::Client.new(client_options)
+    client.update!(session[:authorization])
+
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.authorization = client
+
+    @event_list = service.list_events(params[:calendar_id])
+  end
+  def new_event
+    client = Signet::OAuth2::Client.new(client_options)
+    client.update!(session[:authorization])
+
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.authorization = client
+
+    today = Date.today
+
+    event = Google::Apis::CalendarV3::Event.new({
+      start: Google::Apis::CalendarV3::EventDateTime.new(date: today),
+      end: Google::Apis::CalendarV3::EventDateTime.new(date: today + 1),
+      summary: 'New event!'
+    })
+
+    service.insert_event(params[:calendar_id], event)
+
+    redirect_to events_url(calendar_id: params[:calendar_id])
+  end
+
 
  
   def edit
@@ -62,6 +135,16 @@ class EventsController < ApplicationController
   end
 
   private
+  def client_options
+    {
+      client_id: Rails.application.secrets.google_client_id,
+      client_secret: Rails.application.secrets.google_client_secret,
+      authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+      token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+      scope: Google::Apis::CalendarV3::AUTH_CALENDAR,
+      redirect_uri: 'http://localhost:3000/callback'
+    }
+  end
 
     def event_params
       params.require(:event).permit(:name, :title, :content, :user_id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,16 @@ Rails.application.routes.draw do
     sessions: 'devise/sessions'
   }
   
+
+ 
   devise_scope :user do
     authenticated :user do
       root 'events#index', as: :authenticated_root
     end
-
+    get '/redirect', to: 'events#redirect', as: 'redirect'
+    get '/callback', to: 'events#callback', as: 'callback'
+    get '/calendars', to: 'events#calendars', as: 'calendars'
+    post '/events/:calendar_id', to: 'events#new_event', as: 'new_event', calendar_id: /[^\/]+/
     resources :users
     resources :events
     # get "/events/list/all" => 'events#list'
